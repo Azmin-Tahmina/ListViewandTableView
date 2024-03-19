8d238eaeb2255ad7a6916e4a37a95c8b57044aa3
Azmin-Tahmina <123420809+Azmin-Tahmina@users.noreply.github.com>
2024-03-19 16:05:07 -0600

    Created 20240319

---
+++ ListEx1/ListEx1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListEx1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SampleListView.ItemsSource = new ObservableCollection<Item>
+            {
+                new Item { Name = "Item 1" },
+                new Item { Name = "Item 2" },
+                new Item { Name = "Item 3" },
+                new Item { Name = "Item 4" }
+            };
+        }
+
+        private void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if(e.Item != null && e.Item is Item item)
+            {
+                DisplayAlert("Item Tapped", item.Name, "OK");
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+    }
+
+}
