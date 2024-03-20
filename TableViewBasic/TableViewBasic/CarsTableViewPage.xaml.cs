8459c2a08f04af3563936392038e0a9107bd99e3
Azmin-Tahmina <123420809+Azmin-Tahmina@users.noreply.github.com>
2024-03-20 13:14:08 -0600

    Simple Table view with CS and XAML

---
+++ TableViewBasic/TableViewBasic/CarsTableViewPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TableViewBasic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarsTableViewPage : ContentPage
+    {
+        public CarsTableViewPage()
+        {
+            InitializeComponent();
+            Title = "Cars Table";
+            var tableView = new TableView
+            {
+                Intent = TableIntent.Form,
+                Root = new TableRoot("Car Details")
+                {
+                    new TableSection("Car 1")
+                    {
+                        new TextCell { Text = "Make", Detail = "Ford" },
+                        new TextCell { Text = "Model", Detail = "Mustang" },
+                        new TextCell { Text = "Color", Detail = "Red" }
+                    },
+                    new TableSection("Car 2")
+                    {
+                        new TextCell { Text = "Make", Detail = "Chevrolet" },
+                        new TextCell { Text = "Model", Detail = "Camaro" },
+                        new TextCell { Text = "Color", Detail = "Blue" }
+                    }
+                    // Add more cars as needed
+                }
+            };
+
+            // Adding the TableView to the Page
+            Content = tableView;
+        }
+    }
+}
